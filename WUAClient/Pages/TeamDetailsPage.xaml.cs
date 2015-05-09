b85aec0b53245d7ff39ef9cfb85c92ea6b5ac292
Otto Remse <otto.remse@evry.com>
2015-05-09 21:14:52 +0200

    Fixed formating of returnscreen and handling for mass-return

--- WUAClient/Pages/TeamDetailsPage.xaml.cs
+++ WUAClient/Pages/TeamDetailsPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,6 +16,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using HardwareInventory.Datamodel;
 using HardwareInventory.Repository;
+using HardwareInventory.Utilities;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,7 +28,8 @@ namespace HardwareInventory.Pages
     public sealed partial class TeamDetailsPage : Page
     {
         private TeamLoanViewModel viewModel;
-        private string TeamName;
+        public static string TeamName;
+        private IEnumerable<LoanItem> _selectedLoanItems = new List<LoanItem>();
         public TeamDetailsPage()
         {
             this.InitializeComponent();
@@ -34,29 +37,62 @@ namespace HardwareInventory.Pages
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             TeamName = e.Parameter as string;
+            teamNameTextBlock.Text = TeamName;
             await RefreshViewModel();
         }
 
-        public async Task RefreshViewModel()
+        private async Task RefreshViewModel()
         {
             var result = (await LoanRepository.GetLoanItems(TeamName)).GroupBy(x => x.LoanedBy);
             loanList.ItemsSource = result;
         }
-        public async void ReturnAllItems(object sender, RoutedEventArgs e)
+     
+        private async void ReturnItemsForUser(object sender, RoutedEventArgs e)
         {
-            var button = sender as Button;
+            var button = sender as HyperlinkButton;
             var source = button.DataContext as IGrouping<string, LoanItem>;
             var items = source.ToList();
-           
-            await LoanRepository.ReturnItems(items);
-            await RefreshViewModel();
+            await ReturnItems(items);
         }
 
-        public async void ReturnSingleItem(object sender, RoutedEventArgs e)
+        private async void ReturnSelected(object sender, RoutedEventArgs e)
         {
-            var button = sender as Button;
-            var item = button.DataContext as LoanItem;
-            await LoanRepository.ReturnItem(item);
+            var selectedLoanItems = (from ListItem in VisualTreeTraverser.FindVisualChildren<ListViewItem>(loanList)
+                                     where ListItem.IsSelected
+                                     select ListItem.Content as LoanItem).ToList();
+            await ReturnItems(selectedLoanItems);
+
+        }
+
+        private async Task ReturnItems(IEnumerable<LoanItem> loanItems)
+        {
+            var selectedLoanItems = loanItems as LoanItem[] ?? loanItems.ToArray();
+
+            //add to public variable for commandinvoker to be aware of it.
+            _selectedLoanItems = selectedLoanItems;
+            var contentString = string.Join(Environment.NewLine,
+                selectedLoanItems.Select(x => $"Team: {x.TeamName} User:{x.LoanedBy} Item: {x.Item.Name}"));
+
+            var messageDialog = new MessageDialog(contentString, "Really return the following items?");
+
+            messageDialog.Commands.Add(new UICommand(
+                "Yes",
+                CommandInvokedHandler));
+            messageDialog.Commands.Add(new UICommand(
+                "No",
+                CommandInvokedHandler));
+            // Set the command that will be invoked by default
+            messageDialog.DefaultCommandIndex = 0;
+
+            // Set the command to be invoked when escape is pressed
+            messageDialog.CancelCommandIndex = 1;
+            await messageDialog.ShowAsync();
+        }
+        private async void CommandInvokedHandler(IUICommand command)
+        {
+            // Display message showing the label of the command that was invoked
+            if (!string.Equals(command.Label, "Yes", StringComparison.CurrentCultureIgnoreCase)) return;
+            await LoanRepository.ReturnItems(_selectedLoanItems);
             await RefreshViewModel();
         }
     }
---
+++ WUAClient/Utilities/VisualTreeTraverser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace HardwareInventory.Utilities
+{
+    public class VisualTreeTraverser
+    {
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) yield break;
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+                if (child is T)
+                {
+                    yield return (T)child;
+                }
+
+                foreach (T childOfChild in FindVisualChildren<T>(child))
+                {
+                    yield return childOfChild;
+                }
+            }
+        }
+    }
+}
