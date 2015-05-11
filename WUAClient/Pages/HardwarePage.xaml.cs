8efc30b47bb56252efae2c5800d701a219c294a9
Otto Remse <otto.remse@evry.com>
2015-05-11 21:06:24 +0200

    fixed hardwareitem styling and filtering

--- WUAClient/Pages/HardwarePage.xaml.cs
+++ WUAClient/Pages/HardwarePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -10,15 +12,21 @@ namespace HardwareInventory.Pages
 {
     public sealed partial class HardwarePage : Page
     {
+        private IEnumerable<HardwareItem> _items = new List<HardwareItem>(); 
+        private DateTime _lastKeyDownAt = DateTime.MinValue;
+        readonly DispatcherTimer _searchTimer = new DispatcherTimer();
         public HardwarePage()
         {
             this.InitializeComponent();
             RefreshData();
+            _searchTimer.Interval = TimeSpan.FromMilliseconds(300);
+            _searchTimer.Tick += (sender, o) => { FilterData(); };
         }
 
         private async Task RefreshData()
         {
-           itemGrid.ItemsSource = (await HardwareRepository.GetHardwareItems()).OrderBy(x => x.Name).ToList();
+            _items = (await HardwareRepository.GetHardwareItems()).OrderBy(x => x.Name).ToList();
+           itemGrid.ItemsSource = _items;
         }
 
         public void HardwareItem_OnItemClick(object sender, ItemClickEventArgs e)
@@ -30,7 +38,27 @@ namespace HardwareInventory.Pages
         public async void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
             await RefreshData();
+
         }
+
+    
+
+        public void FilterTextBox_TextChanged(object sender, TextChangedEventArgs te)
+        {
+            _searchTimer.Start();
+        }
+
+        private void FilterData()
+        {
+            _searchTimer.Stop();
+            var searchWords = filterTextbox.Text;
+            itemGrid.ItemsSource = string.IsNullOrWhiteSpace(searchWords) ? 
+                _items : _items.Where(x => x.Name.ToLower()
+                .StartsWith(searchWords.ToLower()) || x.Name.Split(' ')
+                 .Any(namePart => namePart.ToLower()
+                 .StartsWith(searchWords.ToLower()))).ToList();
+        }
+
         public void BackButton_Pressed(object sender, RoutedEventArgs e)
         {
             if (Frame.CanGoBack)
